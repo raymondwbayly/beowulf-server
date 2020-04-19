c2def884d3ad9ff9e7f9127f0bc161bdf954f11b
Raymond Bayly <raymond.bayly@outlook.com>
2020-04-19 11:08:05 -0400

    upddate the users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs = require('fs');
 
-/* GET users listing. */
+// Users File for the static data
+var usersJSON = path.join(__dirname, '../lib', 'users.json');
+var userJSON = path.join(__dirname, '../lib', 'user.json');
+
+/* GET Users listing. */
 router.get('/', function(req, res, next) {
-  res.send('STUB: List of Users');
+  var readable = fs.createReadStream(usersJSON);
+  readable.pipe(res);
+});
+
+/* GET Config Object JSON return. */
+router.get('/:uid', function(req, res, next) {
+  var readable = fs.createReadStream(userJSON);
+  // req.params.uid is how to access the ID
+  readable.pipe(res);
 });
 
 module.exports = router;
