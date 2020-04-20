f6bd0da10970f0618a46ca922772397fc1b526cf
Raymond Bayly <raymond.bayly@outlook.com>
2020-04-19 22:20:59 -0400

    update news uri objects
    This adds the news story and single story calls to the story for news.

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs = require('fs');
 
-/* GET Auth listing. */
+// Alerts File for the static data
+var storyJSON = path.join(__dirname, '../lib', 'news-story.json');
+var newsJSON = path.join(__dirname, '../lib', 'news.json');
+
+/* GET News listing. */
 router.get('/', function(req, res, next) {
-  res.send('The News Service');
+  var readable = fs.createReadStream(newsJSON);
+  readable.pipe(res);
+});
+
+/* GET returns news story. */
+router.get('/:uid', function(req, res, next) {
+  var readable = fs.createReadStream(storyJSON);
+  // req.params.uid is how to access the ID
+  readable.pipe(res);
 });
 
 module.exports = router;
