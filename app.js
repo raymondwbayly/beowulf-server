75b7ce6ccdb86ac95b441267249ab132276be859
Raymond Bayly <raymond.bayly@outlook.com>
2020-04-20 03:02:57 -0400

    Update the Authorization
    Update the Authorization

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+//Body Parsing
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET Auth listing. */
-router.get('/', function(req, res, next) {
-  res.send('The Auth Service');
+router.post('/', function(req, res) {
+  var user_id = req.body.id;
+  var token = req.body.token;
+  var geo = req.body.geo;
+  var rtString = '';
+
+  if (token === '2234FFR1RRDF'){
+    rtString = 'Application is Authorized with Token ' + token + '  Application ID: ' + user_id + '  and Location of ' + geo;
+
+  } else {
+    rtString = 'Application is not Authorized the token ' + token + ' Was not in our system';
+  }
+  res.send(rtString);
 });
 
 module.exports = router;
