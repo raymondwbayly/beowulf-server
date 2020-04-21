521fb7347ae4ff9951a4c95a933d9193cbe016bf
Raymond Bayly <raymond.bayly@outlook.com>
2020-04-21 10:42:59 -0400

    update timeszone retrieval

--- routes/lookup.js
+++ routes/lookup.js
@@ -5,11 +5,29 @@ var fs = require('fs');
 var ejs = require('ejs');
 var Records = require('../bin/records');
 
+// Lookup Libraries
+var timezonesPATH = path.join(__dirname, '../lib/lookup', 'timezones.json');
+var timezonesJSON =  require(timezonesPATH);
+
 
 /* GET tasks listing. */
 router.get('/', function(req, res, next) {
     res.send('This is the default service for lookup');
 });
+
+/* GET Time Zones List. */
+router.get('/timezones', function(req, res, next) {
+    var readable = fs.createReadStream(timezonesPATH);
+    readable.pipe(res);
+});
+
+/* GET Retrieve a Time Zone. */
+router.get('/timezone/:uid', function(req, res, next) {
+    res.send(Records.getSingleRecord(timezonesJSON,req.params.uid));
+});
+
+
+
 module.exports = router;
 
 
